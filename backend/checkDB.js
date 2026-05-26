b6262b6b0f88981573725617c92dfc118c28ce91
ashish1136 <ashish1136.be24@chitkarauniversity.edu.in>
2026-05-26 17:19:12 +0530

    removed backend from frontend repo

--- backend/checkDB.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Itinerary = require('./models/Itinerary');
-
-mongoose.connect('mongodb://127.0.0.1:27017/itenary_db').then(async () => {
-    const itins = await Itinerary.find();
-    console.log(JSON.stringify(itins, null, 2));
-    process.exit(0);
-});
